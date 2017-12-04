ebc89d7b1f2decd9c5524b884c21b152d85f81d0
jianvary <754549372@qq.com>
2017-12-04 18:49:46 +0800

    json创建

---
+++ json.js
@@ -0,0 +1,107 @@
+var data = {
+    '2g':{
+        'cpu':'1核',
+        'leichun': '2g',
+        'kbs': '2M',
+        'city': {
+            'beijingJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'beijing': [185,96,0,111],
+            'shanghaiJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'shanghai': [220,270,240,200],
+            'guangzhouJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'guangzhou': [220,270,240,200],
+            'chengdouJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'chengdou': [220,270,240,200],
+        }
+    },
+    '4g':{
+        'cpu':'4核',
+        'leichun': '4g',
+        'kbs': '5M',
+        'city': {
+            'beijingJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'beijing': [185,96,0,111],
+            'shanghaiJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'shanghai': [220,270,240,200],
+            'guangzhouJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'guangzhou': [220,270,240,200],
+            'chengdouJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'chengdou': [220,270,240,200],
+        }
+    },
+    '8g':{
+        'cpu':'4核',
+        'leichun': '8g',
+        'kbs': '10M',
+        'city': {
+            'beijingJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'beijing': [185,96,0,111],
+            'shanghaiJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'shanghai': [220,270,240,200],
+            'guangzhouJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'guangzhou': [220,270,240,200],
+            'chengdouJson':{
+                'ucloud': 185,
+                'aliyun': 96,
+                'tecentyun': 0,
+                'jinshan': 111
+            },
+            'chengdou': [220,270,240,200],
+        }
+    },
+};
